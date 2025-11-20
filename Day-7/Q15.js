225c56e008a7cb65568869e52b88695aa7aa4114
Ankitha R B <ankithabelagundi@gmail.com>
2025-11-20 15:28:33 +0530

    JS

---
+++ Day-7/Q15.js
@@ -0,0 +1,18 @@
+const username = "Ankitha";
+const course = "JavaScript Essentials";
+
+console.log(`Hello ${username}, welcome to the ${course} course!`);
+
+const name = "Sam";
+const age = 21;
+
+const student = {
+  name,
+  age,
+  greet() {
+    console.log("Hello");
+  }
+};
+
+const getFullName = (first, last) => `${first} ${last}`;
+console.log(getFullName("Sam", "Wilson")); // Sam Wilson
