ede05901b69ac54546abb80d7efa9faed1dae243
Ankitha R B <ankithabelagundi@gmail.com>
2025-11-25 22:59:00 +0530

    Function Exceution Context

---
+++ Day-9/Q19.js
@@ -0,0 +1,14 @@
+let age = 25; 
+function displayAge() {
+  console.log("Current age:", age);
+}
+function changeAge() {
+  age = 30; 
+  console.log("Age after update:", age);
+}
+
+
+displayAge(); 
+changeAge(); 
+
+displayAge(); 
